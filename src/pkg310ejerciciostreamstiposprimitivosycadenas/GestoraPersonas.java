8db22911d94c7c32f04c043b8aae85931964c8f0
sergio <sergiohurtadomahecha@gmail.com>
2017-05-06 22:03:12 +0200

    Primer commit en ordenador sony

--- src/pkg310ejerciciostreamstiposprimitivosycadenas/GestoraPersonas.java
+++ src/pkg310ejerciciostreamstiposprimitivosycadenas/GestoraPersonas.java
@@ -33,7 +33,7 @@ public class GestoraPersonas extends ArrayList<Persona>{
         }
         return devolver;
     }
-
+//probando git
     private void EscribirArchivo() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
